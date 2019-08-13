d9c3408a2b84f92ad046b6e94b92d2963c416cf5
Kenechukwu <Allisonkosy@gmail.com>
2019-08-13 09:06:01 +0100

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,7 +2,7 @@ import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { TranslateModule } from '@ngx-translate/core';
-import { ElectronService } from './providers/electron.service';
+import { ElectronService, LeagueService, WindowService } from './providers';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -11,7 +11,7 @@ describe('AppComponent', () => {
         AppComponent
       ],
       providers: [
-        ElectronService
+        ElectronService, LeagueService, WindowService
       ],
       imports: [
         RouterTestingModule,
